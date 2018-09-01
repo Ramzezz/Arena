6ad00a75bd432cc0d8163bab0a285f3aa8ba5604
Ramzezz <leo.ramzell@github.com>
2018-09-01 11:43:06 +0200

    Initial Commit

---
+++ Areana/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Areana
+//
+//  Created by Leo Ramzell on 2018-09-01.
+//  Copyright © 2018 Leo Ramzell. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
